09472041cc75cb146bd2d0a141bb661cb4e88c71
neerajp99 <neerajp1999@gmail.com>
2019-08-13 23:19:34 +0530

    Add mongo schema for URL

---
+++ models/Url.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Scheme = mongoose.Schema;
+
+const UrlSchema = new Schema({
+  initialUrl: {
+    type: String
+  },
+  urlCOde: {
+    type: String
+  },
+  shortenUrl: {
+    type: String
+  },
+  createdAt: {
+    type: String,
+    default: Date.now
+  },
+  updatedAt: {
+    type: String,
+    default: Date.now
+  }
+});
+
+module.exports = Url = mongoose.model("UrlSchema", UrlSchema);
