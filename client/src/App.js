f488499f8cb625eceffd5992159fd0b681cb10b2
neerajp99 <neerajp1999@gmail.com>
2019-08-14 22:24:58 +0530

    Edit App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Homepage from "./components/Homepage";
 
 class App extends Component {
   render() {
-    return <div>Hello world!</div>;
+    return (
+      <div>
+        <Router>
+          <Route exact path="/homepage" render={() => <Homepage />} />
+        </Router>
+      </div>
+    );
   }
 }
 
