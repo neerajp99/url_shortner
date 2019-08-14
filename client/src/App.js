1b27ca8f9e6b64f7bc2b71357354c4d0d52a1ce0
neerajp99 <neerajp1999@gmail.com>
2019-08-14 17:59:07 +0530

    Initial commit with front end

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <div>Hello world!</div>;
+  }
+}
+
+export default App;
