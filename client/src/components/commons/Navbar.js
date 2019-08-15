92920fedeecf79c47f452ae23f8f8c4746955be4
neerajp99 <neerajp1999@gmail.com>
2019-08-16 00:59:22 +0530

    Add Navbar

---
+++ client/src/components/commons/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="container ">
+        <div className=" navbar row navbar navbar-expand-sm navbar-dark ">
+          Navbar
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
