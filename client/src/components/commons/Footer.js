eecdaf62974111c631c1631ec502667b78c9494d
neerajp99 <neerajp1999@gmail.com>
2019-08-16 00:59:06 +0530

    Add footer

---
+++ client/src/components/commons/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="container ">
+        <div className=" footer ">
+          Footer
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
